6bdbdd9dfb1281eb7d9298113aa357982e845229
wlaszkiewicz <nika25.laszkiewicz@gmail.com>
2025-11-07 23:02:43 +0100

    Implement getWorkoutZone and add tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-//        int age = 28;
-//        int bpm = 160;
-//        int maxHR = HRCalculator.calculateMaxHR(age);
-//        String workoutZone = HRCalculator.getWorkoutZone(age, bpm);
-//        System.out.printf("%nage = %d, BPM = %d -> maximum heart rate = %d, workout zone = %s%n", age, bpm, maxHR, workoutZone);
-    }
-}
+//package org.example;
+//
+//public class Main {
+//    public static void main(String[] args) {
+////        int age = 28;
+////        int bpm = 160;
+////        int maxHR = HRCalculator.calculateMaxHR(age);
+////        String workoutZone = HRCalculator.getWorkoutZone(age, bpm);
+////        System.out.printf("%nage = %d, BPM = %d -> maximum heart rate = %d, workout zone = %s%n", age, bpm, maxHR, workoutZone);
+//    }
+//}
